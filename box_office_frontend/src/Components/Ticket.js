e869f52a7479b93282029d7cdd1cabf7284e41a9
L Lerner <shalom2you@gmail.com>
2018-07-23 13:47:31 -0700

    adds functional display of tickets on ticketlist

--- box_office_frontend/src/Components/Ticket.js
+++ box_office_frontend/src/Components/Ticket.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
-
+import '../CSS/Ticket.css';
 
 class Ticket extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			id: 0,
+			title: "",
+			submittedBy: "",
+			dateSubmitted: "",
+			lastUpdated: "",
+			status: ""
+		}
+	}
+
+
   render() {
     return (
-      <div className="Ticket">
+      <div className="ticketdiv">
+      	<span className="ticket">{this.props.title}</span>
+      	<span className="ticket">{this.props.submittedBy}</span>
+        <span className="ticket">{this.props.dateSubmitted}</span>
+        <span className="ticket">{this.props.lastUpdated}</span>
+        <span className="ticket">{this.props.status}</span>
       </div>
     );
   }
